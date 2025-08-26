63fb61e7852fd137c0f95e07d803e5d8447ae9c5
reikoyuasa <yuasa.reiko.k5@s.mail.nagoya-u.ac.jp>
2025-08-26 14:24:17 +0900

    [26] 重要度別にチャットの背景色を変更

--- chatapp/src/components/ChatCard.vue
+++ chatapp/src/components/ChatCard.vue
@@ -12,10 +12,26 @@ defineProps({
 </script>
 
 <template>
-  <div>
-    {{ chat }}
+  <div class="border rounded-lg p-4 mb-4 bg-white shadow">
+  <div :class="{highImportant : chat.importance === 3, middleImportant : chat.importance === 2, lowImportant : chat.importance === 1, done : chat.importance === 0}">
+    名前：{{ chat.sender }}<br>
+    {{chat.message}}<br>
+    時刻：{{ chat.date }}
+  </div>
   </div>
 </template>
 
 <style scoped>
+.highImportant {
+  background-color: #ffe5e5; /* 薄い赤色の背景 */
+}
+.middleImportant {
+  background-color: #fff5e5; /* 薄いオレンジ色の背景 */
+}
+.lowImportant {
+  background-color: #e5ffe5; /* 薄い緑色の背景 */
+}
+.done {
+  background-color: #f0f0f0; /* 薄い灰色の背景 */
+}
 </style>

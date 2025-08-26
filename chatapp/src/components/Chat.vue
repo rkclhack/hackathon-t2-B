42920663e279efd074943be0ed9ca7f089ee1d03
yoniha428 <superrope428@gmail.com>
2025-08-26 11:41:02 +0900

    feature-20 idがnullになるのを修正

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -26,7 +26,7 @@ onMounted(() => {
 // 投稿メッセージをサーバに送信する
 const onPublish = () => {
   socket.emit("publishEvent", {
-    id: Math.floor(Math.random * maxId),
+    id: Math.floor(Math.random() * maxId),
     sender: userName.value,
     date: Date.now(),
     message: chatContent.value,

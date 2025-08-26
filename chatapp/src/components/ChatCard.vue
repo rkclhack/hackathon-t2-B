3a836c8f34e7f293e0864501bd5c3434270d979c
kisana-me <yuto01200120yuto@gmail.com>
2025-08-26 17:40:47 +0900

    ADD: チャットコンポーネントのスタイルを改良

--- chatapp/src/components/ChatCard.vue
+++ chatapp/src/components/ChatCard.vue
@@ -1,5 +1,6 @@
 <script setup>
-defineProps({
+import { computed } from "vue"
+const props = defineProps({
   chat: {
     id: String,
     sender: String,
@@ -9,19 +10,51 @@ defineProps({
     importance: Number
   }
 })
+
+// フォームのボタンに適切な重要度のクラスを付与する
+const importanceClass = computed(() => {
+  switch (props.chat.importance) {
+    case 3:
+      return "highImportant"
+    case 2:
+      return "middleImportant"
+    case 1:
+      return "lowImportant"
+    case 0:
+      return "done"
+    default:
+      return ""
+  }
+})
+
+// 日付を見やすい形式に変換
+const formattedDate = new Date(props.chat.date).toLocaleString("ja-JP", {
+  month: "numeric",
+  day: "numeric",
+  hour: "2-digit",
+  minute: "2-digit"
+});
 </script>
 
 <template>
-  <div class="border rounded-lg p-4 mb-4 bg-white shadow">
-  <div :class="{highImportant : chat.importance === 3, middleImportant : chat.importance === 2, lowImportant : chat.importance === 1, done : chat.importance === 0}">
-    <p class="sender">{{ chat.sender }}</p><br>
-    <p class="message">{{ chat.message }}</p><br>
-    <p class="date">{{ new Date(chat.date).toLocaleString() }}</p>
-  </div>
+  <div class="message-wrap" :class="importanceClass">
+    <p class="sender">{{ chat.sender }}</p>
+    <p class="message">{{ chat.message }}</p>
+    <details>
+      <summary>{{ formattedDate }}</summary>
+      <p>ジャンル: {{ chat.genre }}</p>
+      <p>重要度: {{ chat.importance }}</p>
+      <p>ID: {{ chat.id }}</p>
+    </details>
   </div>
 </template>
 
 <style scoped>
+.message-wrap {
+  max-width: 700px;
+  border-radius: 8px;
+  padding: 8px;
+}
 .sender {
   font-weight: bold;
   margin-bottom: 5px;
@@ -32,5 +65,9 @@ defineProps({
 .message {
   white-space: pre-wrap; /* 改行を保持 */
   word-wrap: break-word; /* 長い単語を折り返す */
+}
+.message-wrap summary {
+  color: #6d6d6d;
+  font-size: 12px;
 }
 </style>

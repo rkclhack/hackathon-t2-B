9d84fc05bd74ccce31d14a42ae6a53a629cd70d3
reikoyuasa <yuasa.reiko.k5@s.mail.nagoya-u.ac.jp>
2025-08-26 13:15:02 +0900

    [26] チャットをカード化

---
+++ chatapp/src/components/ChatCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  chat: {
+    id: String,
+    sender: String,
+    date: Number,
+    message: String,
+    genre: Number,
+    importance: Number
+  }
+})
+</script>
+
+<template>
+  <div>
+    {{ chat }}
+  </div>
+</template>
+
+<style scoped>
+</style>

8f064e24ebd6ada544e0659141789ee2bb3f68ef
skr08m <sakuraba.mizuki08@gmail.com>
2025-08-26 14:36:05 +0900

    ログイン画面の変更

--- chatapp/src/App.vue
+++ chatapp/src/App.vue
@@ -3,10 +3,12 @@ import { provide, ref } from "vue"
 
 // #region reactive state
 const userName = ref("")
+const employeeNumber = ref("")
 // #endregion
 
 // #region global variable
 provide("userName", userName)
+provide("employeeNumber", employeeNumber)
 // #endregion
 </script>
 
--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -2,9 +2,11 @@
 import { inject, ref } from "vue"
 import { useRouter } from "vue-router"
 import socketManager from "../socketManager.js"
+import "./login.css"
 
 // #region global state
 const userName = inject("userName")
+const employeeNumber = inject("employeeNumber")
 // #endregion
 
 // #region local variable
@@ -14,20 +16,26 @@ const socket = socketManager.getInstance()
 
 // #region reactive variable
 const inputUserName = ref("")
+const inputEmployeeNumber = ref("")
+const inputPassword = ref("")
 // #endregion
 
 // #region browser event handler
 // 入室メッセージをクライアントに送信する
 const onEnter = () => {
   // ユーザー名が入力されているかチェック
-  if (inputUserName.value === "") {
-    alert("ユーザー名を入力してください。")
+  if (!inputEmployeeNumber.value.trim()) {
+    alert("社員番号を入力してください。")
+    return
+  }
+  if (!inputPassword.value.trim()) {
+    alert("パスワードを入力してください。")
     return
   }
   // 入室情報を送信(既存のチャットを要求)
   socket.emit("loginEvent")
   // 全体で使用するnameに入力されたユーザー名を格納
-  if (userName) userName.value = inputUserName.value
+  if (userName) userName.value = inputEmployeeNumber.value
   // チャット画面へ遷移
   router.push({ name: "chat" })
 }
@@ -35,20 +43,28 @@ const onEnter = () => {
 </script>
 
 <template>
-  <div class="mx-auto my-5 px-4">
-    <h1 class="text-h3 font-weight-medium">Vue.js Chat サンプル</h1>
-    <div class="mt-10">
-      <p>ユーザー名</p>
-      <input type="text" class="user-name-text" v-model="inputUserName"/>
+  <div class="login-container">
+    <h1 class="login-title">Vue.js Chat サンプル</h1>
+
+    <div class="login-form">
+      <label class="login-label">社員番号</label>
+      <input type="text" class="login-input" v-model="inputEmployeeNumber" />
+
+      <label class="login-label">パスワード</label>
+      <input type="password" class="login-input" v-model="inputPassword" />
+
+      <button type="button" class="login-button" @click="onEnter">
+        入室する
+      </button>
     </div>
-    <button type="button" @click="onEnter" class="button-normal">入室する</button>
   </div>
 </template>
 
 <style scoped>
+/*
 .user-name-text {
   width: 200px;
   border: 1px solid #888;
   margin-bottom: 16px;
-}
+}*/
 </style>
